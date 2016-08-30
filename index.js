f0acf4c70a624b828c598f3be361d283146fa9b9
NaokiMaeda <maeda.naoki.md9@is.naist.jp>
2016-08-30 16:04:13 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const {app , BrowserWindow} = require("electron");
+
+let mainWindow = null;
+
+function createWindow(){
+	mainWindow = new BrowserWindow({width : 800 , height : 600});
+	mainWindow.loadURL("file://${_direname}/main.html");
+}
+
+app.on("ready" , createWindow);
+
+// Quit when all windows are closed.
+app.on("window-all-closed", () => {
+	app.quit();
+});
+
+mainWindow.on("close" , function(){
+	mainWindow = null;
+});
