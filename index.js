6095ca0f5dd8ee05995d9bac080d2242b1a7768f
NaokiMaeda <maeda.naoki.md9@is.naist.jp>
2016-09-01 14:42:19 +0900

    Implement Materialize

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const {app , BrowserWindow} = require("electron");
+const path = require("path");
 
 let mainWindow = null;
 
 function createWindow(){
-	mainWindow = new BrowserWindow({width : 800 , height : 600});
-	mainWindow.loadURL("file://${_direname}/main.html");
+	mainWindow = new BrowserWindow({
+		width : 800 , height : 600 ,
+		// webPreferences : {
+		// 	nodeIntegration : false
+		// }
+	});
+	const modelPath = path.join("file://", __dirname,"main.html");
+	mainWindow.loadURL(modelPath);
 }
 
 app.on("ready" , createWindow);
@@ -12,8 +19,4 @@ app.on("ready" , createWindow);
 // Quit when all windows are closed.
 app.on("window-all-closed", () => {
 	app.quit();
-});
-
-mainWindow.on("close" , function(){
-	mainWindow = null;
 });
